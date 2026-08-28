d2ae0235405a8ba22ac21624542fa7754d45a5e5
Jonathan Caprell <jonathan@caprell.com>
2026-08-28 07:52:08 -0400

    Update `MixProject` dependency list [`:ex_doc`]

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Masonree.MixProject do
       deps: [
         {:boundary, "~> 0.10", runtime: false},
         {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-        {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+        {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+        {:ex_doc, "~> 0.40", only: :dev, runtime: false}
       ],
       deps_path: "dep",
       dialyzer: [ignore_warnings: ".dialyzer.exs"],
