b19a4d0eca2aa7ded006038f87353590f2b37ce3
Jonathan Caprell <jonathan@caprell.com>
2026-08-31 19:18:55 -0400

    Create `v0.4.0` release

--- mix.exs
+++ mix.exs
@@ -78,7 +78,7 @@ defmodule Masonree.MixProject do
       name: "Masonree",
       start_permanent: env == :prod,
       test_coverage: [ignore_modules: [~r/\AMasonreeBench(\.|\z)/]],
-      version: "0.3.0"
+      version: "0.4.0"
     ]
   end
 
